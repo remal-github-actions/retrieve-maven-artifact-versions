3c7687c331a8e55139c119e3ac0e099212c00b9f
remal <remal@users.noreply.github.com>
2022-09-08 03:38:14 +0000

    [push-back] Push-back updated files during build

--- dist/index.js
+++ dist/index.js
@@ -575,6 +575,7 @@ async function run() {
     }
     catch (error) {
         core.setFailed(error instanceof Error ? error : error.toString());
+        throw error;
     }
 }
 //noinspection JSIgnoredPromiseFromCall
